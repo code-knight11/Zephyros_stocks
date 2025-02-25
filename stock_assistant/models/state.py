26dda67ef2ec715638a6024b113bec891196cf8d
rachanaaaaa <rachu.aravindan@gmail.com>
2025-02-25 13:31:15 +0530

    Update state.py
    added message context

--- stock_assistant/models/state.py
+++ stock_assistant/models/state.py
@@ -1,6 +1,7 @@
-from typing import Annotated, TypedDict
+from typing import Annotated, TypedDict,Dict,Any
 from langchain_core.messages import AnyMessage
 from langgraph.graph.message import add_messages
 
 class State(TypedDict):
     messages: Annotated[list[AnyMessage], add_messages]
+    context: Dict[str, Any]
