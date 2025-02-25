d4de927f639a73e2aeed0ddcefc9e60d406886d4
rachanaaaaa <rachu.aravindan@gmail.com>
2025-02-25 11:51:53 +0530

    Delete rag directory
    deleting old directory

--- rag/state.py
+++
@@ -1,6 +0,0 @@
-from typing import Annotated, TypedDict
-from langchain_core.messages import AnyMessage
-from langgraph.graph.message import add_messages
-
-class State(TypedDict):
-    messages: Annotated[list[AnyMessage], add_messages]
