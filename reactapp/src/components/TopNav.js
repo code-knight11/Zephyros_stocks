eacdd726df898913bc17449c48b5fb70d725da91
rachanaaaaa <rachu.aravindan@gmail.com>
2025-02-25 13:17:50 +0530

    Update TopNav.js

--- reactapp/src/components/TopNav.js
+++ reactapp/src/components/TopNav.js
@@ -16,11 +16,14 @@ const TopNav = () => {
         darkMode ? "bg-gray-900 text-gray-300" : "bg-neutral-100 text-gray-800"
       }`}
     >
-      {/* Brand / Logo */}
-      <div className="text-2xl font-bold mr-10">
-        <Link to="/">Stockbot</Link>
-      </div>
-      
+      <Link
+        className="text-2xl font-bold font-mono underline flex gap-2 mr-16"
+        to="/dashboard"
+      >
+        <img src="logo.png" className="w-8 h-8" />
+        <p>Stockbot</p>
+      </Link>
+
       {/* Navigation Links */}
       <ul className="flex space-x-6">
         <li className="cursor-pointer hover:text-indigo-500">
@@ -33,11 +36,12 @@ const TopNav = () => {
           <Link to="/portfolio">Portfolio</Link>
         </li>
         <li className="cursor-pointer hover:text-indigo-500">
-          <Link to="/" onClick={handleLogout}>Logout</Link>
+          <Link to="/" onClick={handleLogout}>
+            Logout
+          </Link>
         </li>
       </ul>
 
-      {/* Theme Icon on the right */}
       <div>
         <ThemeIcon />
       </div>
