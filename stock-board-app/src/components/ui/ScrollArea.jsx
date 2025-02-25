119b3f41eaf27b36cf41d353527ad964bc2b1690
rachanaaaaa <rachu.aravindan@gmail.com>
2025-02-25 11:52:19 +0530

    Delete stock-board-app directory
    deleting old directory

--- stock-board-app/src/components/ui/ScrollArea.jsx
+++
@@ -1,7 +0,0 @@
-export function ScrollArea({ className, children, ...props }) {
-    return (
-      <div className={`relative overflow-auto ${className}`} {...props}>
-        {children}
-      </div>
-    );
-  }
